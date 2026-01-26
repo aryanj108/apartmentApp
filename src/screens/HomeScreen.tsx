4256d457af2a021ebbfc66fac00ccf53a2780e8f
Aryan <aryanjalota483@gmail.com>
2026-01-26 16:06:48 -0600

    Price Format Fix

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -120,7 +120,6 @@ function ApartmentCard({ listing, matchScore, onPress, isSaved, onSavePress }) {
                 <Text style={styles.cardDetailText}>{listing.bathrooms} Bath</Text>
               </View>
             </View>
-            <Text style={styles.cardPrice}>${formatPrice(listing.price)}</Text>
           </View>
         </View>
       </View>
